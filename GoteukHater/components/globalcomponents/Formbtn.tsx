2a02b2c5a8568feb3e4ed278d999782634043b81
kmsu44 <kmsu444@naver.com>
2023-02-18 04:15:29 +0900

    feat: 모달 버튼 생성
    모달 버튼 생성 및 예약 페이지 UI 작성

--- GoteukHater/components/globalcomponents/Formbtn.tsx
+++ GoteukHater/components/globalcomponents/Formbtn.tsx
@@ -1,46 +1,86 @@
-import React, {Children, useState, type PropsWithChildren} from 'react';
-import {Text, StyleSheet, View, StyleProp, ViewStyle} from 'react-native';
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {Shadow} from 'react-native-shadow-2';
 import Evillcons from 'react-native-vector-icons/EvilIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {height, scale, width} from '../../config/globalStyles';
+import {BottomSheetModal, BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import BtnScreen from '../../screen/reservationscreen/BtnScreen';
+
 interface FormbtnProps {
   title: string;
   icon: string;
   // calendar, pencil
 }
-const Formbtn: React.FunctionComponent<FormbtnProps> = props => {
-  const [modalVisible, setModalVisible] = useState(false);
+
+const Formbtn: React.FC<FormbtnProps> = props => {
+  const bottomSheetModalRef = React.useRef<BottomSheetModal>(null);
+  const snapPoints = React.useMemo(() => [790 * height], []);
+  const handlePresentModalPress = React.useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const modalclose = React.useCallback(() => {
+    bottomSheetModalRef.current?.dismiss();
+  }, []);
   return (
-    <View style={styles.absolute}>
-      <Shadow
-        offset={[0, 2]}
-        style={styles.reservationbtn}
-        distance={1}
-        startColor="rgba(172, 180, 162, 0.25)">
-        <TouchableOpacity
-          style={styles.row}
-          onPress={() => setModalVisible(!modalVisible)}>
-          <Evillcons
-            name={props.icon}
-            size={28}
-            color="black"
-            style={{marginRight: 8 * width}}
-          />
-          <Text
-            style={{color: 'black', fontSize: 16 * scale, fontWeight: '700'}}>
-            {props.title}
-          </Text>
-          <FontAwesome
-            name="chevron-right"
-            size={12}
-            color="black"
-            style={{marginLeft: 8}}
-          />
-        </TouchableOpacity>
-      </Shadow>
-    </View>
+    <BottomSheetModalProvider>
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        enableContentPanningGesture={false}
+        handleIndicatorStyle={{backgroundColor: 'rgba(60, 60, 67, 0.3)'}}
+        backgroundStyle={{
+          marginTop: 5,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 1,
+            height: 12,
+          },
+          shadowOpacity: 0.58,
+          shadowRadius: 16.0,
+          borderTopRightRadius: 24,
+          borderTopLeftRadius: 24,
+          elevation: 24,
+        }}
+        handleStyle={{
+          marginTop: 5,
+          backgroundColor: '#F6F6F9',
+          borderTopRightRadius: 24,
+          borderTopLeftRadius: 24,
+        }}
+        snapPoints={snapPoints}>
+        <BtnScreen close={modalclose} routename={'ReservationHome'} />
+      </BottomSheetModal>
+      <View style={styles.absolute}>
+        <Shadow
+          offset={[0, 2]}
+          style={styles.reservationbtn}
+          distance={1}
+          startColor="rgba(172, 180, 162, 0.25)">
+          <TouchableOpacity
+            style={styles.row}
+            onPress={handlePresentModalPress}>
+            <Evillcons
+              name={props.icon}
+              size={28}
+              color="black"
+              style={{marginRight: 8 * width}}
+            />
+            <Text
+              style={{color: 'black', fontSize: 16 * scale, fontWeight: '700'}}>
+              {props.title}
+            </Text>
+            <FontAwesome
+              name="chevron-right"
+              size={12}
+              color="black"
+              style={{marginLeft: 8}}
+            />
+          </TouchableOpacity>
+        </Shadow>
+      </View>
+    </BottomSheetModalProvider>
   );
 };
 
---
+++ GoteukHater/components/reservation/TimeselectCard.tsx
@@ -0,0 +1,77 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {height, scale, width} from '../../config/globalStyles';
+import Card from '../globalcomponents/Card';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+interface Propstype {
+  time: string;
+  maxnumber: number;
+  nownumber: number;
+}
+const TimeselectCard: React.FC<Propstype> = props => {
+  const FullBox = () => (
+    <View style={[styles.timebox, styles.full]}>
+      <Ionicons
+        name={'alarm-outline'}
+        size={16}
+        color="white"
+        style={{marginRight: 4 * scale}}
+      />
+      <Text style={styles.time}>{props.time}</Text>
+    </View>
+  );
+  const Box = () => (
+    <View style={[styles.timebox]}>
+      <Text style={styles.time}>{props.time}</Text>
+    </View>
+  );
+
+  return (
+    <TouchableOpacity>
+      <Card style={styles.card}>
+        {props.maxnumber === props.nownumber ? <FullBox /> : <Box />}
+        <Text style={styles.numinfo}>
+          {props.nownumber}명 / {props.maxnumber}명
+        </Text>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    paddingTop: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingBottom: 0,
+    width: 114 * scale,
+    marginBottom: 16 * scale,
+  },
+  timebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 30 * scale,
+    borderTopLeftRadius: 15 * scale,
+    borderTopRightRadius: 15 * scale,
+    padding: 8 * scale,
+    backgroundColor: '#20B358',
+  },
+  full: {
+    backgroundColor: '#FF6961',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  time: {
+    color: 'white',
+    fontWeight: '700',
+  },
+  numinfo: {
+    textAlign: 'center',
+    fontSize: 10 * scale,
+    fontWeight: '700',
+    color: '#8B8B8B',
+    margin: 4 * scale,
+  },
+});
+export default TimeselectCard;
