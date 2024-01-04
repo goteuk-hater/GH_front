1f4bcad72be541d8121b43cf13cf66ec87b4d95d
Ivan <ivan@broj.company>
2024-01-05 03:30:47 +0900

    ​refactor : 정보게시판 스크롤 성능 개선

--- GoteukHater/src/screen/bookinformation/BookSearchScreen.tsx
+++ GoteukHater/src/screen/bookinformation/BookSearchScreen.tsx
@@ -1,4 +1,4 @@
-import React, {memo, useCallback, useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   TextInput,
@@ -6,13 +6,15 @@ import {
   TouchableOpacity,
   Keyboard,
   ScrollView,
+  FlatList,
+  VirtualizedList,
+  VirtualizedListProps,
 } from 'react-native';
 import {globalStyle, height, width} from '@/config/globalStyle';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import BookCard from '../../components/booksearch/BookCard';
 import {SelectHeader} from '../../components/booksearch/SelectHeader';
 import {Book} from '../../config/Type';
-import {FlatList} from 'react-native';
 import {useBottomSheetModal} from '@gorhom/bottom-sheet';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../store/RootReducer';
@@ -20,135 +22,111 @@ import StyledText from '@/components/global/StyledText';
 import {useNavigation} from '@react-navigation/native';
 
 const BookSearchScreen = () => {
-  const navigation = useNavigation();
   const [tagList, setTagList] = useState<string[]>([]);
   const [text, setText] = React.useState('');
-  const [loading, setLoading] = useState(false);
   const data = useSelector((state: RootState) => state.Books.data);
   const addTag = (tag: string) => {
-    let newtagList = [...tagList];
-    if (newtagList.includes(tag)) {
+    let newTagList = [...tagList];
+    if (newTagList.includes(tag)) {
       return;
     }
-    newtagList.push(tag);
-    setTagList(newtagList);
+    newTagList.push(tag);
+    setTagList(newTagList);
     Keyboard.dismiss();
   };
   const removeTag = (tag: string) => {
-    let newtagList = [...tagList];
-    if (!newtagList.includes(tag)) {
+    let newTagList = [...tagList];
+    if (!newTagList.includes(tag)) {
       return;
     }
-    newtagList.splice(newtagList.indexOf(tag), 1);
-    setTagList(newtagList);
+    newTagList.splice(newTagList.indexOf(tag), 1);
+    setTagList(newTagList);
     Keyboard.dismiss();
   };
 
   const [filteredData, setFilteredData] = useState(Array<Book>);
   const filter = () => {
-    let newdata = [...data];
+    let newData = [...data];
     if (tagList.length === 0 && text === '') {
-      setFilteredData(newdata);
+      setFilteredData(newData);
       return;
     }
     if (tagList.length !== 0) {
-      newdata = newdata.filter(book => {
+      newData = newData.filter(book => {
         return tagList.includes(book.category.category);
       });
     }
     if (text !== '') {
-      newdata = newdata.filter(book => {
+      newData = newData.filter(book => {
         return book.title.includes(text);
       });
     }
-    setFilteredData(newdata);
+    setFilteredData(newData);
   };
   const {dismissAll} = useBottomSheetModal();
-  const flatListRef = React.useRef<ScrollView>(null);
+  const flatListRef = React.useRef<FlatList>(null);
   const scrollToTop = () => {
-    flatListRef.current?.scrollTo({
-      y: 0,
-      animated: true,
-    });
+    flatListRef.current?.scrollToOffset({animated: true, offset: 0});
   };
   useEffect(() => {
     scrollToTop();
   }, [filteredData]);
 
-  const [renderCount, setRenderCount] = useState(10);
   useEffect(() => {
     filter();
-    setRenderCount(8);
   }, [tagList, text]);
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#F6F6F9',
-      }}>
-      <View style={styles.container}>
-        <View style={styles.searchbox}>
-          <EvilIcons name="search" size={20} color="black" />
-          <TextInput
-            style={[globalStyle.p2, styles.input]}
-            onChangeText={text => {
-              setText(text);
-            }}
-            onFocus={() => {
-              dismissAll();
-            }}
-            value={text}
-            placeholder="검색어를 입력해 주세요."
-            placeholderTextColor={'#818181'}
-          />
-          <TouchableOpacity
-            onPress={() => {
-              setText('');
-            }}>
-            <EvilIcons name="close" size={20} color="black" />
-          </TouchableOpacity>
-        </View>
-        <SelectHeader
-          tagList={tagList}
-          addTag={addTag}
-          removeTag={removeTag}
-          setTagList={setTagList}
+    <View style={styles.container}>
+      <View style={styles.searchbox}>
+        <EvilIcons name="search" size={20} color="black" />
+        <TextInput
+          style={[globalStyle.p2, styles.input]}
+          onChangeText={text => {
+            setText(text);
+          }}
+          onFocus={() => {
+            dismissAll();
+          }}
+          value={text}
+          placeholder="검색어를 입력해 주세요."
+          placeholderTextColor={'#818181'}
         />
+        <TouchableOpacity
+          onPress={() => {
+            setText('');
+          }}>
+          <EvilIcons name="close" size={20} color="black" />
+        </TouchableOpacity>
       </View>
-      <ScrollView
+      <SelectHeader
+        tagList={tagList}
+        addTag={addTag}
+        removeTag={removeTag}
+        setTagList={setTagList}
+      />
+      <FlatList
         ref={flatListRef}
-        onScroll={event => {
-          const offsetY = event.nativeEvent.contentOffset.y;
-          const contentHeight = event.nativeEvent.contentSize.height;
-          if (offsetY >= contentHeight - 600 * height) {
-            setRenderCount(renderCount + 8);
-          }
-        }}
-        contentContainerStyle={styles.scrollView}
-        scrollEventThrottle={16}
-        style={{marginTop: 12 * height}}>
-        {filteredData.length === 0 && (
-          <StyledText style={[globalStyle.p1, {textAlign: 'center'}]}>
-            검색 결과가 없습니다.
-          </StyledText>
-        )}
-        {filteredData.map((book, index) => {
-          if (index >= renderCount) {
-            return;
-          }
-          return (
-            <View style={styles.wrapCard} key={book.title}>
-              <BookCard Book={book} />
-            </View>
-          );
-        })}
-      </ScrollView>
+        data={filteredData}
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        renderItem={({item}) => <BookCard Book={item} />}
+        ItemSeparatorComponent={() => <View style={{height: 12 * height}} />}
+        keyExtractor={(item, index) => item.title}
+        numColumns={2}
+        initialNumToRender={4}
+        maxToRenderPerBatch={4}
+        windowSize={6}
+        updateCellsBatchingPeriod={10}
+        ListFooterComponent={
+          <View style={{height: 12 * height, width: '100%'}} />
+        }
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     paddingHorizontal: 16 * width,
     marginTop: 12 * height,
     rowGap: 12 * height,
@@ -174,8 +152,8 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginLeft: 16 * width,
+    gap: 12 * width,
+    marginBottom: 12 * height,
   },
 });
 
